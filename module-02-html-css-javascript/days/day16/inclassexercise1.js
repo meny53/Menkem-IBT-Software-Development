c0a25908e86a82e4163c12cd97c507c571706798
meny53 <menkemzekarias@gmail.com>
2026-08-24 12:25:34 +0300

    feat: day 14

--- module-02-html-css-javascript/days/day16/inclassexercise1.js
+++ module-02-html-css-javascript/days/day16/inclassexercise1.js
@@ -1,25 +1,40 @@
 const bill = '500';
 const billNum = Number(bill);
-if (billNum >= 300){tip = billNum * 0.1}
-else{tip = billNum * 0.05}
-var total = billNum + tip
-var fee = 0;
-const method = 'cbe birr'
-switch(method){
+
+let tip;
+
+if (billNum >= 300) {
+    tip = billNum * 0.1;
+} else {
+    tip = billNum * 0.05;
+}
+
+const total = billNum + tip;
+
+let fee = 0;
+const method = 'cbe birr';
+
+switch (method) {
     case 'telebirr':
-        fee = total * 0.05
+        fee = total * 0.05;
         break;
+
     case 'cbe birr':
-        fee = total * 0.01
+        fee = total * 0.01;
         break;
+
     default:
-        fee = total * 0.02
+        fee = total * 0.02;
 }
-const totalwithfee = billNum + tip + fee;
+
+const totalwithfee = total + fee;
+
 const name = ['jhon', 'jane', 'jack', 'jill', 'jerry'];
-for (let i = 0; i < name.length; i++){
+
+for (let i = 0; i < name.length; i++) {
     const amount = totalwithfee / name.length;
-    console.log(`name: ${name[i]},`);
+    console.log(`Name: ${name[i]}, Payment: ${amount}`);
 }
+
 console.log(`fee: ${fee}`);
 console.log(`total with fee: ${totalwithfee}`);
